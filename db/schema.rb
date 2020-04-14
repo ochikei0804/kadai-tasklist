71d4627568e268fb1d4d792d5a27e9058a883a21
ochikei <kei.060804@icloud.com>
2020-04-14 13:24:11 +0000

    kadai-column-add

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_034327) do
+ActiveRecord::Schema.define(version: 2020_04_13_131704) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
