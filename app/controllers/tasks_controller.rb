ebc1ffb7de665e46a5a2c5996e21a04f5e91428a
ochikei <kei.060804@icloud.com>
2020-04-27 13:02:04 +0000

    paginate,index修正版

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
     before_action :require_user_logged_in
     
     def index
-        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
     end
     
     def show
